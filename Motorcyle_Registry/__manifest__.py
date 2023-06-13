c28419693b765f06268ecf3fd9c54bcc6b91c48e
visv-odoo <visv@odoo.com>
2023-06-13 14:21:07 -0400

    Finished custom module

---
+++ Motorcyle_Registry/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Motorcycle Registry',
+    'summary': """Manage Registration of Motorcycles""",
+    'description': """Motorcycle Registry
+    ====================
+    This Module is used to keep track of the Motorcycle Registration and Ownership of each motorcycled of the brand.
+    """,
+    'license': 'OPL-1',
+    'author': 'visv-odoo',
+    'website': 'https://github.com/visv-odoo/Training-Exerisise-motorbike-odoo.git',
+    'category': 'Kawiil',
+    'depends': ['base'],
+    'data': [],
+    'demo': [],
+    'application': True,
+}
